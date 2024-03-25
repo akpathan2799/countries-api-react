c04cfb2419616fc3bacec5f1b112f94c7afb1a37
Adeebkhan Pathan <akpathan2799@gmail.com>
2024-03-25 14:42:40 +0500

    commit

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -5,7 +5,7 @@ const CountryCard = ({image,countryName,population,capital,region}) => {
   return (
 
     <Link to={`/country/:${countryName}`}>
-    <div className="flex flex-col h-full w-96 rounded-md border border-black dark:border-white bg-white dark:bg-slate-900 shadow-lg transform transition hover:scale-110 self-baseline shadow-lg max-[850px]:w-[300px] max-[450px]:w-[320px] ">
+    <div className="flex flex-col h-full w-72 rounded-md border border-black dark:border-white bg-white dark:bg-slate-900 shadow-lg transform transition hover:scale-110 self-baseline shadow-lg max-[850px]:w-[300px] max-[450px]:w-[320px] ">
   <img src={image} alt={`Flag of ${countryName}`} className="w-full h-52 object-fill flex-grow"/>
   <div className="p-2 text-left flex flex-col justify-between">
     <p className="text-black dark:text-white text-3xl font-semibold my-3">{countryName}</p>
